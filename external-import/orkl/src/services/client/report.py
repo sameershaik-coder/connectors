13aece446b87b6e329783b51ca57d85c2765cd5b
sameershaik-coder <sameershaik.coder@gmail.com>
2024-03-08 14:43:34 +0530

    removed unused module references

--- external-import/orkl/src/services/client/report.py
+++ external-import/orkl/src/services/client/report.py
@@ -1,12 +1,6 @@
 from .api import ORKLAPIClient
-from services.utils.configVariables import ConfigOrkl
-import os
-from services.utils import get_json_object_from_file
+
 
 class ReportClient(ORKLAPIClient):
     
-    def get_version_sync_done(self):
-        root_dir = os.path.dirname(os.path.abspath(__file__))
-        file_path = "sync_details.json"
-        file_path = os.path.join(root_dir, file_path)
-        return get_json_object_from_file(file_path,"version_sync_done")
+    pass
